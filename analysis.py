78c73a29702059b9affcf23cf1329eb82233f21f
Kyrstn Hall <kyrstn.hall@gmail.com>
2024-03-10 15:57:31 -0700

    num clauses can handle multiple sentences

--- analysis.py
+++ analysis.py
@@ -104,34 +104,43 @@ def type_token_ratio(text):
 # finds the root token of a sentence, usually the main verb
 # in instances there is a dependent clause, it is the verb of the independent clause
 def find_root_of_sentence(doc):
-    root_token = None
     for token in doc:
-        if (token.dep_ == "ROOT"):
-            root_token = token
-    return root_token
+        if token.dep_ == "ROOT":
+            return token
 
 # find the other verbs in the sentence
 def find_other_verbs(doc, root_token):
     other_verbs = []
     for token in doc:
-        ancestors = list(token.ancestors)
-        if (token.pos_ == "VERB" and len(ancestors) == 1\
-            and ancestors[0] == root_token):
-            other_verbs.append(token)
+        if token.pos_ == "VERB" and token != root_token:
+            ancestors = list(token.ancestors)
+            if len(ancestors) == 1 and ancestors[0] == root_token:
+                other_verbs.append(token)
     return other_verbs
 
 # find the token spans for each verb
 def get_clause_token_span_for_verb(verb, doc, all_verbs):
-    first_token_index = len(doc)
-    last_token_index = 0
-    this_verb_children = list(verb.children)
-    for child in this_verb_children:
-        if (child not in all_verbs):
-            if (child.i < first_token_index):
-                first_token_index = child.i
-            if (child.i > last_token_index):
-                last_token_index = child.i
-    return(first_token_index, last_token_index)
+    first_token_index = verb.i
+    last_token_index = verb.i
+
+    for token in doc:
+        if token in all_verbs:
+            continue
+
+        if token in list(verb.children):
+            if token.i < first_token_index:
+                first_token_index = token.i
+            if token.i > last_token_index:
+                last_token_index = token.i
+
+        elif token.pos_ in ["CCONJ", "PUNCT"]:
+            if token.i < verb.i:
+                first_token_index = token.i + 1
+            else:
+                last_token_index = token.i
+                break
+
+    return (first_token_index, last_token_index + 1)
 
 # REQUIREMENT 5 - Number of clauses
 @app.route("/num_clauses", methods=["POST"])
@@ -160,39 +169,70 @@ def num_clauses(text):
         if current_clause:
             clauses.append(current_clause)
         '''
-    
-    doc = nlp(text)
 
-    # calls function
-    root_token = find_root_of_sentence(doc)
+        '''
+        # taken from https://subscription.packtpub.com/book/data/9781838987312/2/ch02lvl1sec13/splitting-sentences-into-clauses
+        doc = nlp(text)
 
-    # use preceding function to find the remaining
-    other_verbs = find_other_verbs(doc, root_token)
+        # calls function 
+        root_token = find_root_of_sentence(doc)
+        print(f"root_token: {root_token}")
 
-    # put together all the verbs in one array
-    token_spans = []   
-    all_verbs = [root_token] + other_verbs
-    for other_verb in all_verbs:
-        (first_token_index, last_token_index) = \
-        get_clause_token_span_for_verb(other_verb, 
-                                        doc, all_verbs)
-        token_spans.append((first_token_index, 
-                            last_token_index))
-        
-    sentence_clauses = []
-    for token_span in token_spans:
-        start = token_span[0]
-        end = token_span[1]
-        if (start < end):
-            clause = doc[start:end]
-            sentence_clauses.append(clause)
-    sentence_clauses = sorted(sentence_clauses, 
-                            key=lambda tup: tup[0])
+        # use preceding function to find the remaining
+        other_verbs = find_other_verbs(doc, root_token)
+        print(f"other verbs: {other_verbs}")
 
-    clauses_text = [clause.text for clause in sentence_clauses]
-    print(f"clauses_text: {clauses_text}")
+        # put together all the verbs in one array
+        token_spans = []   
+        all_verbs = [root_token] + other_verbs
+        for other_verb in all_verbs:
+            (first_token_index, last_token_index) = \
+            get_clause_token_span_for_verb(other_verb, 
+                                            doc, all_verbs)
+            token_spans.append((first_token_index, 
+                                last_token_index))
+            
+        print(f"token_spans: {token_spans}")
+            
+        sentence_clauses = []
+        for token_span in token_spans:
+            start = token_span[0]
+            end = token_span[1]
+            if (start < end):
+                clause = doc[start:end]
+                sentence_clauses.append(clause)
+        sentence_clauses = sorted(sentence_clauses, 
+                                key=lambda tup: tup[0])
 
-    return len(clauses_text)
+        clauses_text = [clause.text for clause in sentence_clauses]
+        print(f"clauses_text: {clauses_text}")
+
+        return len(clauses_text)
+        '''
+        all_clauses = []
+        sentences = [sent.text for sent in nlp(text).sents]
+
+        for sentence in sentences:
+            doc = nlp(sentence)
+            root_token = find_root_of_sentence(doc)
+            other_verbs = find_other_verbs(doc, root_token)
+            all_verbs = [root_token] + other_verbs
+
+            token_spans = []
+            for verb in all_verbs:
+                token_span = get_clause_token_span_for_verb(verb, doc, all_verbs)
+                token_spans.append(token_span)
+
+            sentence_clauses = []
+            for token_span in token_spans:
+                start, end = token_span
+                clause = doc[start:end].text
+                sentence_clauses.append(clause)
+
+            all_clauses.extend(sentence_clauses)
+
+    #print(f"num clauses: {len(all_clauses)}")
+    return len(all_clauses)
 
 # REQUIREMENT 8 - Verb errors
 @app.route("/verbErr", methods=["POST"])
